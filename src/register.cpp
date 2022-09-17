065035a2b9d4727f79153dfccbde0b0e6b06984e
FurWaz <fur.waz06@gmail.com>
2022-09-17 23:06:43 +0200

    Exported extensions methods

---
+++ src/register.cpp
@@ -0,0 +1,20 @@
+#include <napi.h>
+#include "./exports/ExtensionServer.hpp"
+
+void registerFunction(Napi::Object exports, Napi::Env env, std::string name, Napi::Value (*function)(const Napi::CallbackInfo&))
+{
+    exports.Set(Napi::String::New(env, name), Napi::Function::New(env, function));
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    registerFunction(exports, env, "extensions_start", Extensions_Start);
+    registerFunction(exports, env, "extensions_stop", Extensions_Stop);
+    registerFunction(exports, env, "extensions_getState", Extensions_GetState);
+    registerFunction(exports, env, "extensions_onExtensionUpdate", Extensions_OnExtensionUpdate);
+    registerFunction(exports, env, "extensions_getExtensions", Extensions_GetExtensions);
+
+    return exports;
+}
+
+NODE_API_MODULE(FullBowody, Init);
