3e2abb2034d725e0358c7f00094f38aba4158615
FurWaz <fur.waz06@gmail.com>
2022-10-13 18:57:07 +0200

    Added personnal useful scripts

--- copy.bat
+++ copy.bat
@@ -1,3 +1,4 @@
 @copy .\bridge ..\FullBowody\src\server\Core\bridge\
 @copy .\build\Release\fullbowody.node ..\FullBowody\src\server\Core\build\fullbowody.node
 @copy .\build\Release\fullbowody.pdb ..\FullBowody\src\server\Core\build\fullbowody.pdb
+@copy .\build\Release\*.dll ..\FullBowody\dist_electron
---
+++ setBins.bat
@@ -0,0 +1,1 @@
+@copy .\src\bin .\build\Release\
