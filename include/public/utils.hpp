deffad8eecba48c1af7ad57c22bc0019bc67310f
FurWaz <fur.waz06@gmail.com>
2024-05-28 20:53:31 +0200

    Fck DLLExport sht, going back to old school technique

--- include/public/utils.hpp
+++ include/public/utils.hpp
@@ -1,8 +1,8 @@
 #if (defined WIN32 || defined _WIN32 || defined WINCE)
     #ifdef BUILDING_DLL
-        # define DLLExport __declspec(dllexport)
+        # define DLLExport // __declspec(dllexport)
     #else
-        # define DLLExport __declspec(dllimport)
+        # define DLLExport // __declspec(dllimport)
     #endif
 #else
 # define DLLExport
