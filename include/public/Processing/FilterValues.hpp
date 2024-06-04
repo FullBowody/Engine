ba2f7b0104f4401f8aa1bf059761b8f0db3430cf
FurWaz <fur.waz06@gmail.com>
2024-06-04 08:30:11 +0200

    used templates for filterValues and added filter threshold parameter

--- include/public/Processing/FilterValues.hpp
+++ include/public/Processing/FilterValues.hpp
@@ -15,14 +15,63 @@ namespace FilterValues
          * The higher the value, the more extreme values will be taken into account.
         */
         float SDThreshold = 1.0f;
+
+        /**
+         * @brief The threshold for the maximum amount of filtred values.
+         * Default value is 0.5f, this means that if more than 50% of the values are filtered,
+         * the result will fallback to the average.
+         * The higher the value, the more values will be able to be filtered.
+         * Values equal or behond 1.0f will force a fallback to the average.
+        */
+        float filterThreshold = 0.5f;
     } FilterSettings;
 
-    Vec3f filter(const std::vector<Vec3f>& values);
-    Vec3f filter(const std::vector<Vec3f>& values, FilterSettings settings);
+    template<typename T> T
+    filter(const std::vector<T>& values, FilterSettings settings = FilterSettings());
 
-    Quaternion filter(const std::vector<Quaternion>& values);
-    Quaternion filter(const std::vector<Quaternion>& values, FilterSettings settings);
+    Pose filter(const std::vector<Pose>& values, FilterSettings settings = FilterSettings())
+    {
+        std::vector<Vec3f> positions;
+        std::vector<Quaternion> rotations;
 
-    Pose filter(const std::vector<Pose>& values);
-    Pose filter(const std::vector<Pose>& values, FilterSettings settings);
+        for (Pose p : values)
+        {
+            positions.push_back(p.getPosition());
+            rotations.push_back(p.getRotation());
+        }
+
+        return Pose(
+            filter(positions, settings),
+            filter(rotations, settings)
+        );
+    }
+    
+    template<typename T>
+    T filter(const std::vector<T>& values, FilterSettings settings)
+    {
+        T average = T();
+        for (T v : values) average += v;
+        average /= values.size();
+
+        float variance = 0;
+        for (T v : values) variance += (v - average).length2();
+        variance /= values.size();
+
+        float standardDeviationBiased = sqrt(variance) * settings.SDThreshold;
+        T res = T();
+        int count = 0;
+        for (T v : values)
+        {
+            if ((v - average).length() < standardDeviationBiased)
+            {
+                res += v;
+                count++;
+            }
+        }
+
+        if (count < values.size() * settings.filterThreshold)
+            return average;
+
+        return res / count;
+    }
 };
