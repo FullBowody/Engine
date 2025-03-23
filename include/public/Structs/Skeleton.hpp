3c3fa4832f73f4b53eadc00a2ca1729b15c907e2
FurWaz <fur.waz06@gmail.com>
2025-03-23 12:46:27 +0100

    Changed skeleton joint to enum

--- include/public/Structs/Skeleton.hpp
+++ include/public/Structs/Skeleton.hpp
@@ -8,26 +8,28 @@
 class ENGINE_API Skeleton : public Identifiable
 {
 public:
-    static const int JOINT_HIP = 0;
-    static const int JOINT_SPINE = 1;
-    static const int JOINT_CHEST = 2;
-    static const int JOINT_NECK = 3;
-    static const int JOINT_HEAD = 4;
-    static const int JOINT_SHOULDER_L = 5;
-    static const int JOINT_SHOULDER_R = 6;
-    static const int JOINT_ELBOW_L = 7;
-    static const int JOINT_ELBOW_R = 8;
-    static const int JOINT_WRIST_L = 9;
-    static const int JOINT_WRIST_R = 10;
-    static const int JOINT_HAND_L = 11;
-    static const int JOINT_HAND_R = 12;
-    static const int JOINT_HIP_L = 13;
-    static const int JOINT_HIP_R = 14;
-    static const int JOINT_KNEE_L = 15;
-    static const int JOINT_KNEE_R = 16;
-    static const int JOINT_ANKLE_L = 17;
-    static const int JOINT_ANKLE_R = 18;
-    static const int NB_JOINTS = 19;
+    enum SkeletonJoint {
+        JOINT_HIP = 0,
+        JOINT_SPINE = 1,
+        JOINT_CHEST = 2,
+        JOINT_NECK = 3,
+        JOINT_HEAD = 4,
+        JOINT_SHOULDER_L = 5,
+        JOINT_SHOULDER_R = 6,
+        JOINT_ELBOW_L = 7,
+        JOINT_ELBOW_R = 8,
+        JOINT_WRIST_L = 9,
+        JOINT_WRIST_R = 10,
+        JOINT_HAND_L = 11,
+        JOINT_HAND_R = 12,
+        JOINT_HIP_L = 13,
+        JOINT_HIP_R = 14,
+        JOINT_KNEE_L = 15,
+        JOINT_KNEE_R = 16,
+        JOINT_ANKLE_L = 17,
+        JOINT_ANKLE_R = 18,
+        NB_JOINTS = 19
+    };
 
 private:
     std::array<Joint, NB_JOINTS> joints;
